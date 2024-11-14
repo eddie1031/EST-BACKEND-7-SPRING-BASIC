e977ee9dc7e84c8830e530e9a22833d9fdb6d8c7
Eddie Woo <archaco123@gmail.com>
2024-11-15 00:13:06 +0900

    2강 - 요리 시뮬레이터, 일본 요리능력 추가

--- src/main/java/est/spring/basic/Chief.java
+++ src/main/java/est/spring/basic/Chief.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Chief {
 
-    private KoreanCuisineAbility ability = new KoreanCuisineAbility();
+//    private KoreanCuisineAbility ability = new KoreanCuisineAbility();
+    private JapaneseCuisineAbility ability = new JapaneseCuisineAbility();
 
     public void cook(List<String> ingredient) {
         ability.apply(ingredient);
